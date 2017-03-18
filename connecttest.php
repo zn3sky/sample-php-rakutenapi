18779113926ef0abae4f53226d47684d0079c468
zn3sky <zn3sky@gmail.com>
2017-03-18 16:27:31 +0900

    create

---
+++ connecttest.php
@@ -0,0 +1,27 @@
+<?php
+// 接続情報定義
+require_once("Define.php");
+// "Define.php"には下記を設定
+//define("RAKUTEN_DEVELOPERID", "楽天ディベロッパーID");
+//define("RAKUTEN_AFFILIATEID", "楽天アフィリエイトID");
+
+require_once ("vendor/autoload.php");
+
+$client = new RakutenRws_Client();
+$client->setApplicationId(RAKUTEN_DEVELOPERID);
+$client->setAffiliateId(RAKUTEN_AFFILIATEID);
+
+// IchibaItem/Search API から、keyword=トレンチコート を検索します
+$response = $client->execute(
+	"IchibaItemSearch",
+	array(
+	"keyword" => "トレンチコート"
+	)
+);
+
+if ($response->isOk()) {
+	var_dump($response);
+} else {
+	echo "Error:".$response->getMessage();
+}
+?>
